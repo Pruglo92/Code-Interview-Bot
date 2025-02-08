bc7a510027225d9d4334f47ccda75044ca85b78a
aleksandr <aleksandrkashapov59@gmail.com>
2025-02-08 16:59:02 +0500

    Fix #15

--- src/main/java/ru/study/codeinterviewbot/service/impl/JavaSectionServiceImpl.java
+++ src/main/java/ru/study/codeinterviewbot/service/impl/JavaSectionServiceImpl.java
@@ -2,6 +2,7 @@ package ru.study.codeinterviewbot.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.study.codeinterviewbot.entity.JavaSection;
 import ru.study.codeinterviewbot.repository.JavaSectionRepository;
 import ru.study.codeinterviewbot.service.JavaSectionService;
@@ -9,6 +10,7 @@ import ru.study.codeinterviewbot.service.JavaSectionService;
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class JavaSectionServiceImpl implements JavaSectionService {
 
